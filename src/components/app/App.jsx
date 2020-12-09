895d10743dc16df7ce9ac7aacb0cea43a87c335d
Patrick Wilson <pmwilson333@gmail.com>
2020-12-08 16:47:17 -0800

    router working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Header from '../header/Header';
+import Home from '../home/Home';
+import Projects from '../projects/Projects';
+import About from '../about/About';
+import Contact from '../contact/Contact';
+import Footer from '../footer/Footer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={Home}
+          />
+          <Route
+            path="/projects"
+            exact
+            component={Projects}
+          />
+          <Route
+            path="/about"
+            exact
+            component={About}
+          />
+          <Route
+            path="/contact"
+            exact
+            component={Contact}
+          />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
